48a057b9afb677df2408b27190010caba101e64f
Raiane Castro <raianecastrodemoura@gmail.com>
2024-03-14 10:44:10 -0300

    creating a portfolio

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1
+      className="text-black
+   text-3xl"
+    >
+      Olá, me chamo Raiane, Front End Developer{" "}
+    </h1>
+  );
+}
+
+export default App;
